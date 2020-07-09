60c7f3bfa0505de659d79f5519d1b08130920e37
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-07-10 05:11:38 +0700

    [doc] Add note about forking (RT#124222); mention Sys::RunUntil, Proc::Govern, IPC::Run

--- lib/Timeout/Self.pm
+++ lib/Timeout/Self.pm
@@ -1,7 +1,14 @@
 package Timeout::Self;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
+
+# IFUNBUILT
+use strict;
+use warnings;
+# END IFUNBUILT
 
 sub import {
     my $package = shift;
@@ -18,7 +25,8 @@ sub import {
 In a script:
 
  # run for at most 30 seconds
- use Timeout::Self qw(30);
+ use Timeout::Self 30;
+ # do stuffs
 
 From the command line:
 
@@ -27,7 +35,22 @@ From the command line:
 
 =head1 DESCRIPTION
 
-This module simply installs a $SIG{ALRM} that dies, and an alarm() call with a
-certain value.
+This module lets you set a time limit on program execution, by installing a
+handler in C<< $SIG{ALRM} >> that simply dies, and then calling C<alarm()> with
+the specified number of seconds.
+
+Caveat: it doesn't play perfectly nice with programs that fork. While the alarm
+handler gets cloned to the child process by Perl, the alarm is not set again so
+the child process will not time out. You can call alarm() again in the child
+process if you want to timeout the child too.
+
+
+=head1 SEE ALSO
+
+L<Sys::RunUntil> can timeout your script by number of clock seconds or CPU
+seconds. It performs C<fork()> at the beginning of program run.
+
+Timing out a process can also be done by a supervisor process, for example see
+L<Proc::Govern>, L<IPC::Run> (see C<timeout()>).
 
 =cut
